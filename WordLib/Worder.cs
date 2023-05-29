3e11129007b4306380a37ac4ab58c8f963235eaf
Daniel Berkowicz <daniel.berkowicz1@gmail.com>
2023-05-29 16:54:53 +0200

    Added methods based on the test-methods

--- WordLib/Worder.cs
+++ WordLib/Worder.cs
@@ -4,7 +4,34 @@
     {
         public bool Palindrome(string input)
         {
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                throw new Exception("No data in input string");
+            }
 
+            string reversed = new string(input.ToLower().ToCharArray().Reverse().ToArray());
+
+            if (input.Equals(reversed))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool FiveOrMoreChar(string input)
+        {
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                throw new Exception("No data in input string");
+            }
+
+            if (input.Length >= 5)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
