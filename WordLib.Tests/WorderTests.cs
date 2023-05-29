d38dd299a11f88ff7646131466abd1d59bd38899
Daniel Berkowicz <daniel.berkowicz1@gmail.com>
2023-05-29 16:47:20 +0200

    Added Theory and test for minimum of five char

--- WordLib.Tests/WorderTests.cs
+++ WordLib.Tests/WorderTests.cs
@@ -3,7 +3,7 @@ namespace WordLib.Tests
     public class WorderTests
     {
         [Fact]
-        public void IsPalindrom()
+        public void IsPalindromFact()
         {
             //Arrange
             Worder worder = new Worder();
@@ -11,6 +11,54 @@ namespace WordLib.Tests
 
             //Act
             bool actual = worder.Palindrome("stats");
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("stats", true)]
+        [InlineData("computer", false)]
+        [InlineData("noon", true)]
+        [InlineData("monkey", false)]
+        public void IsPalindromTheory(string x, bool expected)
+        {
+            //Arrange
+            Worder worder = new Worder();
+
+            //Act
+            bool actual = worder.Palindrome(x);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsMinimumFiveCharFact()
+        {
+            //Arrange
+            Worder worder = new Worder();
+            bool expected = true;
+
+            //Act
+            bool actual = worder.FiveOrMoreChar("stats");
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("stats", true)]
+        [InlineData("computer", true)]
+        [InlineData("noon", false)]
+        [InlineData("monkey", true)]
+        public void IsMinimumFiveCharTheory(string x, bool expected)
+        {
+            //Arrange
+            Worder worder = new Worder();
+
+            //Act
+            bool actual = worder.FiveOrMoreChar(x);
 
             //Assert
             Assert.Equal(expected, actual);
